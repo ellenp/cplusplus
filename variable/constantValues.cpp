5006a2c4c018e951d6c57df24f41f056cdb16ac8
ellenp <pates.ellen@gmail.com>
2016-09-19 23:29:12 +0800

    Create constantValues.cpp

---
+++ variable/constantValues.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define PI 3.14159265359
+#define E 2.71828
+
+const double pi = 3.14159265359;
+const double e = 2.71828;
+
+int main()
+{
+	cout << "PI = " << PI << endl;
+	cout << "pi = " << pi << endl;
+	cout << "E = " << E << endl;
+	cout << "e = " << e << endl;
+}
